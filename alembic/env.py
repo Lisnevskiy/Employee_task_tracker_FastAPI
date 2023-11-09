54493870f8f70e2890b405b5ce6f9ee27c624d15
Alexander <Lisnevskiy14@gmail.com>
2023-11-09 12:11:15 +0300

    Fix env.py

--- alembic/env.py
+++ alembic/env.py
@@ -7,14 +7,14 @@ from alembic import context
 
 from app.models.employee import Employee
 from app.models.task import Task
-from config import POSTRGRESQL_DATABASE_URL
+from config import POSTGRESQL_DATABASE_URL
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
 section = config.config_ini_section
-config.set_section_option(section, "POSTRGRESQL_DATABASE_URL", POSTRGRESQL_DATABASE_URL)
+config.set_section_option(section, "POSTGRESQL_DATABASE_URL", POSTGRESQL_DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
